2c2e3aa29abd44168f3c47cfec19f289b06ddec3
gorkemu <gorkemunal88@gmail.com>
2023-03-15 03:21:45 +0300

    fix style, use react-icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,22 @@
-import "./nav.css";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 const Sidebar = () => {
   return (
-    <aside className="fixed flex flex-col justify-center w-20 h-screen bg-black text-sm z-10 ">
+    <aside className="fixed hidden md:flex flex-col justify-center w-20 h-screen bg-black text-sm z-10 ">
       <ul className="flex flex-col items-center justify-center cursor-pointer">
-        <li className="p-3 opacity-90 hover:opacity-100 transform transition-colors duration-200">
-          <a href="#about">About</a>
+        <li className="p-5 hover:text-yellow hover:-translate-y-1 text-xl transform transition-all duration-200">
+          <a href="https://github.com/gorkemu" target="_blank" rel="noreferrer">
+            <FiGithub />
+          </a>
         </li>
-        <li className="p-3 opacity-90 hover:opacity-100 transform transition-colors duration-200">
-          <a href="#projects">Projects</a>
-        </li>
-        <li className="p-3 opacity-90 hover:opacity-100 transform transition-colors duration-200">
-          <a href="#experience">Experience</a>
+        <li className="p-5  hover:text-yellow hover:-translate-y-1 text-xl transform transition-all duration-200">
+          <a
+            href="https://www.linkedin.com/in/gorkemunal/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiLinkedin />
+          </a>
         </li>
       </ul>
     </aside>
