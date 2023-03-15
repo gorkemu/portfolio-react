39e10227df0c563a2b3c14f3076a8d35e996127b
gorkemu <gorkemunal88@gmail.com>
2023-03-15 03:21:13 +0300

    use react icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./nav.css";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 const Nav = () => {
   const [hState, sethState] = useState("top");
@@ -24,19 +25,34 @@ const Nav = () => {
   return (
     <nav
       className={
-        "header col-span-2 fixed w-full flex items-center justify-between px-20 bg-black text-sm z-20 " +
+        "header col-span-2 fixed w-full flex items-center justify-between md:justify-end p-20 bg-black text-sm z-20 " +
         hState
       }
     >
-      <div className="p-2">Logo</div>
+      <ul className="flex md:hidden items-center justify-start cursor-pointer">
+        <li className="p-4 text-base hover:text-yellow transform transition-all duration-200">
+          <a href="https://github.com/gorkemu" target="_blank" rel="noreferrer">
+            <FiGithub />
+          </a>
+        </li>
+        <li className="p-4 text-base hover:text-yellow transform transition-all duration-200">
+          <a
+            href="https://www.linkedin.com/in/gorkemunal/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiLinkedin />
+          </a>
+        </li>
+      </ul>
       <ul className="flex items-center justify-between cursor-pointer">
-        <li className="py-2 px-3 opacity-90 hover:opacity-100 transform transition-colors duration-200">
+        <li className="py-2 px-3 hover:text-yellow text-lg transform transition-all duration-200">
           <a href="#about">About</a>
         </li>
-        <li className="py-2 px-3 opacity-90 hover:opacity-100 transform transition-colors duration-200">
+        <li className="py-2 px-3 hover:text-yellow text-lg transform transition-all duration-200">
           <a href="#projects">Projects</a>
         </li>
-        <li className="py-2 px-3 opacity-90 hover:opacity-100 transform transition-colors duration-200">
+        <li className="py-2 px-3 hover:text-yellow text-lg transform transition-all duration-200">
           <a href="#experience">Experience</a>
         </li>
       </ul>
