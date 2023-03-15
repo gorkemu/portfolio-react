1af091f2247e1894d114c9fd2ef6359ba75f84e0
gorkemu <gorkemunal88@gmail.com>
2023-03-15 03:20:35 +0300

    fix style

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about" className="pt-24 m-4 h-screen">
-      <div className="flex w-full justify-center items-center">
-        <div className="mx-6">
+    <section id="about" className="px-4 pt-24 md:pt-12 min-h-screen">
+      <div className="flex flex-col md:flex-row gap-10 justify-center items-center">
+        <div className="flex-1 w-3/4 mx-auto">
           <img
             src="https://pbs.twimg.com/profile_images/1624130756829233153/ZEBsJDiR_400x400.jpg"
             className="rounded-full  border-yellow border-2"
             alt=""
           ></img>
         </div>
-        <div>
-          <p className="text-yellow">Hi there, my name is</p>
+        <div className="flex-1">
+          <span className="text-yellow text-xl">Hi there, my name is</span>
           <p className="text-5xl my-2 font-semibold">Görkem Ünal</p>
           <p className="text-2xl">
             A web developer with a focus on the MERN stack, passionate about
